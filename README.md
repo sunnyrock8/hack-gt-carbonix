90f8a5b4525ef531dd4a85dc7cb4e61c2c30fe8a
sunnyrock8 <aarush.yadav47@gmail.com>
2024-09-28 22:21:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hack-gt-carbonix
+
+Carbonix is the path forward to reducing carbon emissions by bringing carbon credit rewards to the individual, gamified in-app to create an engaging experience that rewards sustainable behaviour.
---
+++ lib/theme/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color text = Color.fromRGBO(23, 23, 23, 1.0);
+  static const Color green = Color.fromRGBO(196, 220, 52, 1.0);
+  static const Color darkGreen = Color.fromRGBO(144, 197, 104, 1.0);
+  static const Color blue = Color.fromRGBO(90, 158, 251, 1.0);
+  static const Color colorBackground = Color(0xFF111110);
+  static const Color colorNavShadow = Color(0x45454545);
+  static const Color colorHint = Color(0xFF878787);
+}
